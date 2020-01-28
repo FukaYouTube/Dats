730547fef7864c3adcb17d79449e00c2196f2323
Fuka <weendibol21300@gmail.com>
2020-01-28 13:23:31 +0600

    cc

--- service/admin.service.js
+++ service/admin.service.js
@@ -50,7 +50,7 @@ exports.view_user = async ctx => {
     let users = `Пользователь ${'@' + user.username || user.first_name}: \n\n`
     
     if(user){
-        users += `ID пользователя: ${user._id} \nНик: ${user.first_name} \nЮзер: @${user.username} \nИмя: ${user.user_name} \nФамилия: ${user.user_surname} \nОтчество: ${user.user_middlename} \nДата рождения: ${user.user_birthday} (дата/месяц/год или дата-месяц-год) \nИИН: ${user.iin} \nРеферальная ссылка: ${user.ref_url} \nОт кого: ${user.by_whom} \n Дата регистраций: ${user.date} \n\nДерево пользователя: \n\n`
+        users += `ID пользователя: ${user._id} \nНик: ${user.first_name} \nЮзер: @${user.username} \nИмя: ${user.user_name} \nФамилия: ${user.user_surname} \nОтчество: ${user.user_middlename} \nДата рождения: ${user.user_birthday} (дата/месяц/год или дата-месяц-год) \nИИН: ${user.user_iin} \nРеферальная ссылка: ${user.ref_url} \nОт кого: ${user.by_whom} \n Дата регистраций: ${user.date} \n\nДерево пользователя: \n\n`
 
         let all_user = await User.find({ by_whom: user._id })
         
