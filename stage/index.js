d4fd6a7480d9f985aa4bd02917cc7a9c2f137597
Fuka <weendibol21300@gmail.com>
2020-02-22 10:54:34 +0600

    + register send message stage

--- stage/index.js
+++ stage/index.js
@@ -2,5 +2,6 @@ const Stage = require('telegraf/stage')
 const stage = new Stage()
 
 stage.register(require('./register.stage'))
+stage.register(require('./sendMessage.stage'))
 
 module.exports = stage
