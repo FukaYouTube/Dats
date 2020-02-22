041f1ef439981e3cd7c79312ddf1039ec9087278
Fuka <weendibol21300@gmail.com>
2020-02-22 17:24:28 +0600

    comment photo alboms

--- service/main.service.js
+++ service/main.service.js
@@ -7,13 +7,13 @@ exports.cmdStart = async ctx => {
     let message = JSON.parse(fs.readFileSync(`source/messages/msg.${ctx.session.lang || "ru"}.json`))
     let user = await User.findById(ctx.from.id)
 
-    let mediaArr = [{
-        type: 'photo',
-        media: 'AgACAgIAAxkBAAIHn15ONb7V7f6HaqVvtTQBJD8C9E9dAAJWrTEbhgdxStAwRT48JU2ghXfBDwAEAQADAgADbQADtbEEAAEYBA'
-    }, {
-        type: 'photo',
-        media: 'AgACAgIAAxkBAAIHoF5ONb5ADL7CmXrx1cK_OuN6Mp1OAAJXrTEbhgdxSte0agvz0mtGSrl7kS4AAwEAAwIAA20AA1YXAAIYBA'
-    }]
+    // let mediaArr = [{
+    //     type: 'photo',
+    //     media: 'AgACAgIAAxkBAAIHn15ONb7V7f6HaqVvtTQBJD8C9E9dAAJWrTEbhgdxStAwRT48JU2ghXfBDwAEAQADAgADbQADtbEEAAEYBA'
+    // }, {
+    //     type: 'photo',
+    //     media: 'AgACAgIAAxkBAAIHoF5ONb5ADL7CmXrx1cK_OuN6Mp1OAAJXrTEbhgdxSte0agvz0mtGSrl7kS4AAwEAAwIAA20AA1YXAAIYBA'
+    // }]
 
     let refurl = await User.findById(ctx.startPayload)
     if(refurl && !user){
@@ -23,16 +23,16 @@ exports.cmdStart = async ctx => {
     }
 
     if(user){
-        ctx.replyWithMediaGroup(mediaArr)
-        setTimeout(() => {
+        // ctx.replyWithMediaGroup(mediaArr)
+        // setTimeout(() => {
             ctx.replyWithMarkdown(message["welcome"]["old-user"], keyboard(message["menu"]).oneTime().resize().extra())
-        }, 1000)
+        // }, 1000)
     }else{
-        ctx.replyWithMediaGroup(mediaArr)
-        setTimeout(() => {
+        // ctx.replyWithMediaGroup(mediaArr)
+        // setTimeout(() => {
             ctx.replyWithMarkdown(message["welcome"]["new-user"])
             ctx.scene.enter('new-user')
-        }, 1000)
+        // }, 1000)
     }
 }
 
